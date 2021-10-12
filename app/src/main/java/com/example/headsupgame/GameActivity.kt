21980469f92f18381cf3ae9858453c69b7726cf1
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-10-13 00:37:44 +0300

    changed the interface + added sounds

--- app/src/main/java/com/example/headsupgame/GameActivity.kt
+++ app/src/main/java/com/example/headsupgame/GameActivity.kt
@@ -1,13 +1,19 @@
 package com.example.headsupgame
 
+import android.app.Dialog
 import android.content.res.Configuration
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.view.Window
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlin.random.Random
@@ -19,13 +25,15 @@ import java.lang.reflect.Type
 
 class GameActivity : AppCompatActivity() {
 
-    lateinit var timerTextView: TextView
+    lateinit var timerLandscapeTextView: TextView
+    lateinit var timerPortraitTextView: TextView
     lateinit var celebrityNameTextView: TextView
     lateinit var taboo1TextView: TextView
     lateinit var taboo2TextView: TextView
     lateinit var taboo3TextView: TextView
     lateinit var landscapeLinearLayout: LinearLayout
     lateinit var portraitLinearLayout: LinearLayout
+    lateinit var gameActivityLinearLayout: ConstraintLayout
 
 
     var celebritiesList = arrayListOf<Celebrities.Celebrity>()
@@ -41,35 +49,47 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        //only light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)//keep the app always on light mode
 
+        //hide the action bar
+        supportActionBar?.hide()
 
         val gson = Gson()
         val type: Type = object : TypeToken<List<Celebrities.Celebrity?>?>() {}.type
-        val celebrities: List<Celebrities.Celebrity> = gson.fromJson<List<Celebrities.Celebrity>>(intent.getStringExtra("celebritiesList"), type)
+        val celebrities: List<Celebrities.Celebrity> = gson.fromJson<List<Celebrities.Celebrity>>(
+            intent.getStringExtra("celebritiesList"),
+            type
+        )
         for (celebrity in celebrities) {
             celebritiesList.add(celebrity)
         }
 
+        gameActivityLinearLayout = findViewById(R.id.cl_gameActivity)
         landscapeLinearLayout = findViewById(R.id.ll_gameLandScape)
         portraitLinearLayout = findViewById(R.id.ll_gamePortrait)
-        timerTextView = findViewById<TextView>(R.id.tv_timer)
+        timerLandscapeTextView = findViewById<TextView>(R.id.tv_timerLandscape)
+        timerPortraitTextView = findViewById<TextView>(R.id.tv_timerPortrait)
         celebrityNameTextView = findViewById<TextView>(R.id.tv_celebrityName)
         taboo1TextView = findViewById<TextView>(R.id.tv_taboo1)
         taboo2TextView = findViewById<TextView>(R.id.tv_taboo2)
         taboo3TextView = findViewById<TextView>(R.id.tv_taboo3)
 
         setUI()
-
-
+        val maxVolume = 100.0f
+        val currentVolume = 40.0f
+        val bgMusicMediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext, R.raw.bg_music)
+        bgMusicMediaPlayer.setVolume(currentVolume/maxVolume,currentVolume/maxVolume)
+        bgMusicMediaPlayer.start()
     }
 
     private fun startGame() {
-            startTimer()
-            playerCelebritiesList = randomCelebritiesList()
-            celebrityNameTextView.text = playerCelebritiesList[round].name
-            taboo1TextView.text = playerCelebritiesList[round].taboo1
-            taboo2TextView.text = playerCelebritiesList[round].taboo2
-            taboo3TextView.text = playerCelebritiesList[round].taboo3
+        startTimer()
+        playerCelebritiesList = randomCelebritiesList()
+        celebrityNameTextView.text = playerCelebritiesList[round].name
+        taboo1TextView.text = playerCelebritiesList[round].taboo1
+        taboo2TextView.text = playerCelebritiesList[round].taboo2
+        taboo3TextView.text = playerCelebritiesList[round].taboo3
     }
 
     private fun nextCelebrity() {
@@ -89,14 +109,17 @@ class GameActivity : AppCompatActivity() {
     private fun setUI() {
         val orientation = resources.configuration.orientation
         if (orientation === Configuration.ORIENTATION_LANDSCAPE) {
-            if(!startGameFlag){
+            if (!startGameFlag) {
                 startGameFlag = true
+                round++
                 startGame()
             }
+            gameActivityLinearLayout.setBackgroundResource(R.drawable.bg_landscape_game_activity)
             ll_gameLandScape.isVisible = true
             ll_gamePortrait.isVisible = false
             nextCelebrity()
         } else if (orientation === Configuration.ORIENTATION_PORTRAIT) {
+            gameActivityLinearLayout.setBackgroundResource(R.drawable.bg_portrait_game_activity)
             ll_gameLandScape.isVisible = false
             ll_gamePortrait.isVisible = true
             round++
@@ -120,31 +143,58 @@ class GameActivity : AppCompatActivity() {
 
 
     private fun startTimer() {
+        val tickSoundMediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext, R.raw.human_tick)
+        val winSoundMediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext, R.raw.win)
         object : CountDownTimer(60000, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                timerTextView.text = "" + millisUntilFinished / 1000
+                timerLandscapeTextView.text = "" + millisUntilFinished / 1000
+                timerPortraitTextView.text = "" + millisUntilFinished / 1000
+                tickSoundMediaPlayer.start()
             }
 
             override fun onFinish() {
                 showWinDialog()
+                setUI()
+                winSoundMediaPlayer.start()
             }
         }.start()
     }
 
-    private fun showWinDialog(){
-        val builder = AlertDialog.Builder(this@GameActivity)
-        builder.setMessage("Are you sure you want to delete this entry?")
-            .setCancelable(false)
-            .setPositiveButton("Play Again") { _, _ ->
-                this.recreate()
+    private fun showWinDialog() {
+
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.game_over_dialog)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        val scoreTextView = dialog.findViewById<TextView>(R.id.tv_scoreNumber)
+        val starsImageView = dialog.findViewById<ImageView>(R.id.stars)
+        val returnButton = dialog.findViewById<ImageButton>(R.id.btn_return)
+        val playAgainButton = dialog.findViewById<ImageButton>(R.id.btn_playAgain)
+        val score = (round-1)  * 10
+        scoreTextView.text = score.toString()
+        when {
+            score >= 50 -> {
+                starsImageView.setImageDrawable(getDrawable(R.drawable.three_star))
             }
-            .setNegativeButton("Return") { _, _ ->
-                finish()
+            score >= 30 -> {
+                starsImageView.setImageDrawable(getDrawable(R.drawable.two_star))
             }
-        val alert = builder.create()
-        alert.show()
-//        alert.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(resources.getColor(R.color.purple_200));
+            score == 0 -> {
+                starsImageView.visibility = View.INVISIBLE
+            }
+            else -> {
+                starsImageView.setImageDrawable(getDrawable(R.drawable.one_star))
+            }
+        }
+        returnButton.setOnClickListener {
+            finish()
+        }
+        playAgainButton.setOnClickListener {
+            this.recreate()
+        }
+        dialog.show()
     }
 
 
--- app/src/main/java/com/example/headsupgame/MainActivity.kt
+++ app/src/main/java/com/example/headsupgame/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.headsupgame
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -13,6 +17,15 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_game.*
+import android.media.AudioManager
+
+import android.media.SoundPool
+import android.media.MediaPlayer
+
+
+
+
 
 
 
@@ -20,23 +33,35 @@ import com.google.gson.Gson
 class MainActivity : AppCompatActivity() {
 
     var celebritiesList = arrayListOf<Celebrities.Celebrity>()
-    lateinit var startButton: Button
+    lateinit var startButton: ImageButton
+    lateinit var mainActivityLinearLayout: ConstraintLayout
+    lateinit var startButtonFrameLayout: FrameLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //only light mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)//keep the app always on light mode
 
-        startButton = findViewById<Button>(R.id.btn_start)
-        startButton.isClickable = false
+        //hide the action bar
+        supportActionBar?.hide()
+
+
+        mainActivityLinearLayout = findViewById(R.id.cl_mainActivity)
+        startButtonFrameLayout = findViewById(R.id.fl_startButton)
+        setUI()
+        startButton = findViewById(R.id.btn_start)
+        startButtonFrameLayout.visibility = View.INVISIBLE
         startButton.setOnClickListener {
             val gson = Gson()
             val jsonCelebritiesList: String = gson.toJson(celebritiesList)
             val intent = Intent(this, GameActivity::class.java)
-            intent.putExtra("celebritiesList",jsonCelebritiesList)
+            intent.putExtra("celebritiesList", jsonCelebritiesList)
             startActivity(intent)
         }
         CoroutineScope(Dispatchers.IO).launch {
             getCelebritiesList(onResult = {
-                startButton.isClickable = true
             })
         }
     }
@@ -54,15 +79,30 @@ class MainActivity : AppCompatActivity() {
                     for (Celebrity in response.body()!!) {
                         celebritiesList.add(Celebrity)
                     }
+                    startButtonFrameLayout.visibility = View.VISIBLE
                     onResult()
                 }
 
                 override fun onFailure(call: Call<List<Celebrities.Celebrity>>, t: Throwable) {
-                    startButton.isClickable = false
+                    startButtonFrameLayout.visibility = View.INVISIBLE
                     Toast.makeText(this@MainActivity, "" + t.message, Toast.LENGTH_SHORT).show()
                     onResult()
                 }
             })
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        setUI()
+    }
+
+    private fun setUI() {
+        val orientation = resources.configuration.orientation
+        if (orientation === Configuration.ORIENTATION_LANDSCAPE) {
+            mainActivityLinearLayout.setBackgroundResource(R.drawable.bg_landscape_main_acitvity)
+        } else if (orientation === Configuration.ORIENTATION_PORTRAIT) {
+            mainActivityLinearLayout.setBackgroundResource(R.drawable.bg_portrait_main_acitvity)
         }
     }
 }
